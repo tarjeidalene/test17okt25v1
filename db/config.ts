373049de7983cd3f15836f284dd4f32cfc3776b4
tarjeidalene <173668059+tarjeidalene@users.noreply.github.com>
2025-10-17 14:05:34 +0200

    asdf

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,14 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Bruker = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    fornavn: column.text(),
+    etternavn: column.text(),
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: {Bruker},
 });
